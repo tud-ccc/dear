b1da0a80549c7c9753be0eca21f3af158d63cb4b
Christian Menard <christian.menard@tu-dresden.de>
2020-01-09 16:21:23 +0100

    add a header that makes certain types from APD accessible

---
+++ include/dear/apd_dependencies.hh
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 TU Dresden
+ * All rights reserved.
+ *
+ * Authors:
+ *   Christian Menard
+ */
+
+#pragma once
+
+#include <ara/com/internal/proxy/event.h>
+#include <ara/com/internal/proxy/method.h>
+#include <ara/com/internal/skeleton/event_dispatcher.h>
+#include <ara/com/internal/vsomeip/vsomeip_marshalling.h>
+#include <ara/core/promise.h>
+
+namespace dear {
+namespace apd {
+
+using ara::com::internal::InstanceId;
+using ara::com::internal::vsomeip::common::Deserializer;
+using ara::com::internal::vsomeip::common::Unmarshaller;
+using ara::core::Future;
+using ara::core::Promise;
+using ara::core::Result;
+
+namespace skeleton {
+using ara::com::internal::skeleton::EventDispatcher;
+}
+
+namespace proxy {
+using ara::com::internal::proxy::Event;
+using ara::com::internal::proxy::Method;
+} // namespace proxy
+
+} // namespace apd
+} // namespace dear
